f207cd8d3e5ca7083a6687dc719a3783f95836d5
Alan Lizardi <gil.lizardi@gmail.com>
2025-11-10 18:35:08 -0700

    feat: Implement profile update functionality
    Adds the ability for users to update their profile information, including their name, date of birth, and phone number. This information is validated and then updated in the Firebase Realtime Database.
    
    Also implements the functionality to change the profile picture. Users can select an image from the gallery or take a new one with the camera. The selected image is uploaded to Firebase Storage, and the image URL in the user's profile is updated.
    
    Key changes:
    - Adds validation for user input fields.
    - Implements `actualizarInfo` to save profile data to the database.
    - Implements `subirImagenStorage` and `actualizarImagenBD` for handling profile picture uploads and updates.
    - Wires up the "Actualizar" button to trigger the update process.
    - Refactors permission handling for camera and storage access.

--- app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
+++ app/src/main/java/com/example/appcomprayventa/EditarPerfil.kt
@@ -4,6 +4,7 @@ import android.app.ProgressDialog
 import android.content.ContentValues
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.Menu
@@ -12,19 +13,69 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.appcomprayventa.databinding.ActivityEditarPerfilBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.example.appcomprayventa.databinding.ActivityEditarPerfilBinding
+import com.google.firebase.storage.FirebaseStorage
 
 class EditarPerfil : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditarPerfilBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var progressDialog: ProgressDialog
-    private var imageUri: Uri?= null
+    private var imageUri: Uri? = null
+
+    private var nombres = ""
+    private var f_nac = ""
+    private var codigo = ""
+    private var telefono = ""
+
+    private fun validarInfo(){
+        nombres = binding.EtNombres.text.toString().trim()
+        f_nac = binding.EtFNac.text.toString().trim()
+        codigo = binding.selectorCod.selectedCountryCodeWithPlus
+        telefono = binding.EtTelefono.text.toString().trim()
+
+        if(nombres.isEmpty()){
+            Toast.makeText(this, "Ingrese sus nombres", Toast.LENGTH_SHORT).show()
+        } else if(f_nac.isEmpty()) {
+            Toast.makeText(this, "Ingrese su fecha de nacimiento", Toast.LENGTH_SHORT).show()
+        }else if(codigo.isEmpty()) {
+            Toast.makeText(this, "Ingrese su código de país", Toast.LENGTH_SHORT).show()
+        }else if(telefono.isEmpty()) {
+            Toast.makeText(this, "Ingrese su número de teléfono", Toast.LENGTH_SHORT).show()
+        }else{
+            actualizarInfo()
+        }
+
+    }
+
+    private fun actualizarInfo() {
+        progressDialog.setMessage("Actualizando información")
+        progressDialog.show()
+
+        val hashMap : HashMap<String, Any> = HashMap()
+        hashMap["nombres"] = "${nombres}"
+        hashMap["fecha_nac"] = "${f_nac}"
+        hashMap["codigoTelefono"] = "${codigo}"
+        hashMap["telefono"] = "${telefono}"
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Información actualizada", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e  ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "ERROR: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,24 +85,27 @@ class EditarPerfil : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         progressDialog = ProgressDialog(this)
-        progressDialog.setTitle("Enviando datos...")
+        progressDialog.setTitle("Por favor espere")
         progressDialog.setCanceledOnTouchOutside(false)
 
         cargarInfo()
 
-        binding.FABCambiarImg.setOnClickListener {
-            selec_imagen_de()
+        binding.BtnActualizar.setOnClickListener {
+            validarInfo()
         }
 
 
+        binding.FABCambiarImg.setOnClickListener {
+            selec_imagen_de()
+        }
     }
 
     private fun cargarInfo() {
+
         val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
         ref.child("${firebaseAuth.uid}")
-            .addValueEventListener(object: ValueEventListener{
+            .addValueEventListener( object: ValueEventListener{
                 override fun onDataChange(snapshot: DataSnapshot) {
-
                     val nombres = "${snapshot.child("nombres").value}"
                     val imagen = "${snapshot.child("urlImagenPerfil").value}"
                     val f_nac = "${snapshot.child("fecha_nac").value}"
@@ -63,80 +117,185 @@ class EditarPerfil : AppCompatActivity() {
                     binding.EtFNac.setText(f_nac)
                     binding.EtTelefono.setText(telefono)
 
-                    try {
+                    try{
                         Glide.with(applicationContext)
                             .load(imagen)
                             .placeholder(R.drawable.img_perfil)
                             .into(binding.imgPerfil)
-                    } catch (e:Exception){
-                        Toast.makeText(this@EditarPerfil, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }catch(e: Exception){
+                        Toast.makeText(this@EditarPerfil,
+                            "${e.message}",
+                            Toast.LENGTH_SHORT).show()
+
                     }
 
-                    try {
-                        val codigo = codTelefono.replace("+","").toInt()
+                    try{
+                        val codigo = codTelefono.replace("+", "").toInt()
                         binding.selectorCod.setCountryForPhoneCode(codigo)
-                    } catch (e:Exception){
-                        Toast.makeText(this@EditarPerfil,"${e.message}", Toast.LENGTH_SHORT).show()
-                    }
 
+                    }catch (e: Exception){
+//                        Toast.makeText(this@EditarPerfil,
+//                            "${e.message}",
+//                            Toast.LENGTH_SHORT).show()
+                    }
 
                 }
-
                 override fun onCancelled(error: DatabaseError) {
 
                 }
-            }
-            )
-
-
+            })
     }
 
+    private fun subirImagenStorage(){
+        progressDialog.setMessage("Subiendo imagen de Storage")
+        progressDialog.show()
+
+        val rutaImagen = "ImagenesPerfil/${firebaseAuth.uid}"
+        val storageReference = FirebaseStorage.getInstance().getReference(rutaImagen)
+        storageReference.putFile(imageUri!!)
+            .addOnSuccessListener {takeSnapshot ->
+                val uriTask = takeSnapshot.storage.downloadUrl
+                while(!uriTask.isSuccessful);
+                val urlImagenCargada = "${uriTask.result}"
+                if(uriTask.isSuccessful) {
+                    actualizarImagenBD(urlImagenCargada)
+                }
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()}
+    }
+
+    private fun actualizarImagenBD(urlImagenCargada: String) {
+        progressDialog.setMessage("Actualizando imagen")
+        progressDialog.show()
+
+        val hashMap : HashMap<String, Any> = HashMap()
+        if(imageUri != null){
+            hashMap["urlImagenPerfil"] = urlImagenCargada
+        }
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child("${firebaseAuth.uid}")
+            .updateChildren(hashMap)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                Toast.makeText(this, "Su imagen de perfil se ha actualizado", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e ->
+                progressDialog.dismiss()
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()}
+    }
 
     private fun selec_imagen_de(){
         val popupMenu = PopupMenu(this, binding.FABCambiarImg)
 
-        popupMenu.menu.add(Menu.NONE, 1, 1, "Cámara")
-        popupMenu.menu.add(Menu.NONE, 2, 2, "Galería")
-
+        popupMenu.menu.add(Menu.NONE,1,1, "Camara")
+        popupMenu.menu.add(Menu.NONE,2,2, "Galeria")
         popupMenu.show()
 
         popupMenu.setOnMenuItemClickListener { item ->
             val itemId = item.itemId
+            if(itemId == 1 ){
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    concederPermisosCamara.launch(arrayOf(android.Manifest.permission.CAMERA))
+                }else{
+                    concederPermisosCamara.launch(arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ))
+                }
+            }else if(itemId == 2){
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+                    imagenGaleria()
+                } else{
+                    concederPermisosAlmacenamiento.launch(
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    )
+                }
 
-            if(itemId == 1){
-                //Funcionalidad para camara
-            }else if (itemId == 2){
-                //Funcionalidad para galeria
             }
             return@setOnMenuItemClickListener true
-
-
         }
-
     }
 
     private val concederPermisosCamara =
         registerForActivityResult(
-            ActivityResultContracts.RequestMultiplePermissions()){resultado ->
-            var concedidoTodos = true
-            for (seConcede in resultado.values){
+            ActivityResultContracts.RequestMultiplePermissions()) {
+                resultado -> var concedidoTodos = true
+            for(seConcede in resultado.values) {
                 concedidoTodos = concedidoTodos && seConcede
             }
-
             if (concedidoTodos){
                 imagenCamara()
-            }else{
-                Toast.makeText(this, "No se cuenta con permisos de acceso a la cámara", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(
+                    this,
+                    "El permiso de la camara o almacenamiento se denegaron",
+                    Toast.LENGTH_SHORT
+                ).show()
             }
         }
 
-    private val concederPermisosAlmacenamiento =
+    private val concederPermisosAlmacenamiento=
         registerForActivityResult(
-            ActivityResultContracts.RequestPermission()){esConcedido ->
+            ActivityResultContracts.RequestPermission()) {
+                esConcedido ->
             if (esConcedido){
                 imagenGaleria()
+            }else {
+                Toast.makeText(
+                    this,
+                    "El permiso de almacenamiento se denego",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    private val resultadoCamara_ARL =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ resultado ->
+            if(resultado.resultCode == RESULT_OK){
+                subirImagenStorage()
+//                try{
+//                    Glide.with(this)
+//                        .load(imageUri)
+//                        .placeholder(R.drawable.img_perfil)
+//                        .into(binding.imgPerfil)
+//                }catch(e: Exception){
+//                }
             }else{
-                Toast.makeText(this, "No se cuenta con permisos de acceso al almacenamiento", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "La captura de imagen se canceló",
+                    Toast.LENGTH_SHORT
+                ).show(
+                )
+            }
+        }
+
+    private val resultadoGaleria_ARL =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){ resultado ->
+            if(resultado.resultCode == RESULT_OK){
+                val data = resultado.data
+                imageUri = data!!.data
+                subirImagenStorage()
+
+//                try{
+//                    Glide.with(this)
+//                        .load(imageUri)
+//                        .placeholder(R.drawable.img_perfil)
+//                        .into(binding.imgPerfil)
+//                }catch(e: Exception){
+//                }
+            }else{
+                Toast.makeText(
+                    this,
+                    "La selecció de imagen se canceló",
+                    Toast.LENGTH_SHORT
+                ).show(
+                )
             }
         }
 
@@ -144,58 +303,16 @@ class EditarPerfil : AppCompatActivity() {
         val contentValues = ContentValues()
         contentValues.put(MediaStore.Images.Media.TITLE, "Titulo_imagen")
         contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Descripcion_imagen")
+        imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
 
-       imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues)
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         resultadoCamara_ARL.launch(intent)
     }
 
-    private val resultadoCamara_ARL =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-            resultado ->
-            if (resultado.resultCode == RESULT_OK){
-                try {
-                    Glide.with(this)
-                        .load(imageUri)
-                        .placeholder(R.drawable.img_perfil)
-                        .into(binding.imgPerfil)
-                } catch (e:Exception){
-                    Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Acción Cancelada", Toast.LENGTH_SHORT).show()
-            }
-        }
-
     private fun imagenGaleria() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
-        resultadoAlmacenamiento_ARL.launch(intent)
+        resultadoGaleria_ARL.launch(intent)
     }
-
-    private val resultadoAlmacenamiento_ARL =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
-                resultado ->
-            if (resultado.resultCode == RESULT_OK){
-                val data = resultado.data
-                imageUri = data!!.data
-
-                try {
-                    Glide.with(this)
-                        .load(imageUri)
-                        .placeholder(R.drawable.img_perfil)
-                        .into(binding.imgPerfil)
-                } catch (e:Exception){
-                    Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Acción Cancelada", Toast.LENGTH_SHORT).show()
-            }
-        }
-
 }
-
-
-
-
