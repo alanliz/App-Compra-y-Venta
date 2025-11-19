d263c0e599d35da6e3c1474297051cc9231e9e41
Alan Lizardi <gil.lizardi@gmail.com>
2025-11-18 18:13:38 -0700

    feat: Add initial UI for creating ads
    Adds the initial `CrearAnuncio` activity and its corresponding layout file, `activity_crear_anuncio.xml`.
    
    The new layout provides the user interface for creating a new advertisement. It includes fields for:
    - Image selection (with a RecyclerView to display selected images).
    - Brand, Category, Condition, Price.
    - Title and Description.
    - A "Crear Anuncio" button to submit the form.

---
+++ app/src/main/java/com/example/appcomprayventa/Anuncios/CrearAnuncio.kt
@@ -0,0 +1,17 @@
+package com.example.appcomprayventa.Anuncios
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appcomprayventa.R
+
+class CrearAnuncio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_crear_anuncio)
+
+    }
+}
