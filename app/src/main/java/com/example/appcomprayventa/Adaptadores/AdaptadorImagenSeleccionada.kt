74d743ecaf229ae4aa1d9577dc85063aa3cf9cb0
Alan Lizardi <gil.lizardi@gmail.com>
2025-11-18 18:16:41 -0700

    feat: Add initial selected image adapter
    Adds the initial `AdaptadorImagenSeleccionada` class.

---
+++ app/src/main/java/com/example/appcomprayventa/Adaptadores/AdaptadorImagenSeleccionada.kt
@@ -0,0 +1,12 @@
+package com.example.appcomprayventa.Adaptadores
+
+import android.content.Context
+import com.example.appcomprayventa.Modelo.ModeloImagenSeleccionada
+
+
+class AdaptadorImagenSeleccionada
+    (private val context: Context,
+     private val imagenSelecArrayList : ArrayList<ModeloImagenSeleccionada>
+){
+
+}
